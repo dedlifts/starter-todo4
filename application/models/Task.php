d47ca68a495922cc08d2a8395afedb4e6347e294
Vinz Balitaan <vinzbalitaan@hotmail.com>
2018-03-08 11:44:02 -0800

    Task2 updated.

--- application/models/Task.php
+++ application/models/Task.php
@@ -6,4 +6,40 @@
  * and open the template in the editor.
  */
 
-class Task extends Entity {}
+class Task extends Entity {
+    private $task;
+    private $priority;
+    private $size;
+    private $group;
+    private $field;
+    private $statuses;
+    private $flags;
+    
+    function setTask($task) {
+        $this->task = $task;
+    }
+
+    function setPriority($priority) {
+        $this->priority = $priority;
+    }
+
+    function setSize($size) {
+        $this->size = $size;
+    }
+
+    function setGroup($group) {
+        $this->group = $group;
+    }
+
+    function setField($field) {
+        $this->field = $field;
+    }
+    
+    function setStatuses($statuses) {
+        $this->statuses = $statuses;
+    }
+    
+    function setFlags($flags) {
+        $this->flags = flags;
+    }
+}
