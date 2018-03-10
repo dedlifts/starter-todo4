6cb21482f7625357600cc6f388cdeaa517b0f62a
Nixon Wu <kfwu@hotmail.com>
2018-03-09 17:17:46 -0800

    tasklisttest completed

---
+++ test/application/models/TaskListTest.php
@@ -0,0 +1,27 @@
+// <?php
+
+// /* 
+ // * To change this license header, choose License Headers in Project Properties.
+ // * To change this template file, choose Tools | Templates
+ // * and open the template in the editor.
+ // */
+
+// use PHPUnit\Framework\TestCase;
+
+// class TaskListTest extends TestCase {
+    // private $CI;
+    // private $tasks;
+    // function setUp() {
+        // $this->CI = &get_instance();
+        // $this->CI->load->model('tasks');
+        // $this->tasks = new Tasks();
+    // }
+    
+    // function testValidTasks() {
+        // $count = 0;
+		// $size  = $this->CI->tasks->size();
+		// $all   = $this->CI->tasks->all();
+        // $this->assertEquals(1, $this->task->priority);
+    // }
+    
+// }
