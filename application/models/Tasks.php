6c4c0195180cdd56c9b7bc50480fc2dae27287ed
Vinz Balitaan <vinzbalitaan@hotmail.com>
2018-03-20 16:36:36 -0700

    Job 2 completed

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -6,49 +6,66 @@
  * and open the template in the editor.
  */
 
-class Tasks extends CSV_Model {
+class Tasks extends XML_Model {    
+    public function __construct()
+    {
+        parent::__construct(APPPATH .'../data/tasks.xml', 'id');
+    }
+    
+    protected function load() {
+        if (($tasks = simplexml_load_file($this->_origin)) !== FALSE) {
+            foreach ($tasks as $task) {
+                $record = new stdClass();
+                $record->id = (int) $task['id'];
+                $record->task = (string) $task;
+                $record->priority = (int) $task['priority'];
+                $record->size = (int) $task['size'];
+                $record->group = (int) $task['group'];
+                $record->deadline = (string) $task['deadline'];
+                $record->status = (int) $task['status'];
+                $record->flag = (int) $task['flag'];
 
-        public function __construct()
-        {
-                parent::__construct(APPPATH . '../data/tasks.csv', 'id');
-        }
-        
-        function getCategorizedTasks()
-        {
-            // extract the undone tasks
-            foreach ($this->all() as $task)
-            {
-                if ($task->status != 2)
-                    $undone[] = $task;
+                $this->_data[$record->id] = $record;
             }
-
-            // substitute the category name, for sorting
-            foreach ($undone as $task)
-                $task->group = $this->app->group($task->group);
-
-            // order them by category
-            usort($undone, "orderByCategory");
-
-            // convert the array of task objects into an array of associative objects       
-            foreach ($undone as $task)
-                $converted[] = (array) $task;
-
-            return $converted;
         }
-		
-		// provide form validation rules
-		public function rules()
-		{
-			$config = array(
-				['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
-				['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
-				['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
-				['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
-                                ['field' => 'status', 'label', 'Task status', 'rules' => 'integer|less_than[3]']
-			);
-			return $config;
-		}
+    }
+    
 
+    function getCategorizedTasks()
+    {
+        // extract the undone tasks
+        foreach ($this->all() as $task)
+        {
+            if ($task->status != 2)
+                $undone[] = $task;
+        }
+
+        // substitute the category name, for sorting
+        foreach ($undone as $task)
+            $task->group = $this->app->group($task->group);
+
+        // order them by category
+        usort($undone, "orderByCategory");
+
+        // convert the array of task objects into an array of associative objects       
+        foreach ($undone as $task)
+            $converted[] = (array) $task;
+
+        return $converted;
+    }
+		
+    // provide form validation rules
+    public function rules()
+    {
+            $config = array(
+                    ['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
+                    ['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
+                    ['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
+                    ['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
+                    ['field' => 'status', 'label', 'Task status', 'rules' => 'integer|less_than[3]']
+            );
+            return $config;
+    }
 }
 
 // return -1, 0, or 1 of $a's category name is earlier, equal to, or later than $b's
